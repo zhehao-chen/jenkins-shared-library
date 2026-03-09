a5574650d283c83b29431a63165389d741f809d0
jay.chen <zhehao@uchicago.edu>
2026-03-08 20:07:48 -0700

    Fix containerPushStage: accept (imageName, imageTag, envTag) with Docker login

--- vars/containerPushStage.groovy
+++ vars/containerPushStage.groovy
@@ -1,5 +1,12 @@
-def call(Map config = [:]) {
-    stage('Container Push') {
-        sh 'echo "Pushing container image..."'
+def call(String imageName, String imageTag, String envTag) {
+    echo "=== Container Push Stage ==="
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-credentials',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
+        sh "docker push ${imageName}:${imageTag}"
+        sh "docker push ${imageName}:${envTag}"
     }
 }
